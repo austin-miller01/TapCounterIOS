2afa50ada67bb1b164e961f7fc69e792e0a9cbfa
Austin Miller <ajm19x@my.fsu.edu>
2021-08-06 12:04:39 -0600

    Initial Commit

---
+++ TapCounter/TapCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCounterApp.swift
+//  TapCounter
+//
+//  Created by austin miller on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TapCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
