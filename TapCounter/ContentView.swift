476138ae18a7045de18d30a90924a3a956aadb37
Austin Miller <ajm19x@my.fsu.edu>
2021-08-06 12:29:37 -0600

    Completes my first tap counter

--- TapCounter/ContentView.swift
+++ TapCounter/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count: Int = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Text("Tap Counter").padding().font(.largeTitle)
+            }
+            VStack(alignment: .center){
+                HStack{
+                    Button(action: {
+                        self.count+=1
+                    }){Text("+").padding().foregroundColor(Color.black).font(.system(size: 60)).frame(width: 200, height: 100, alignment: .center).background(Color.green)
+                        
+                    }
+                    
+                }
+                HStack{
+                    Text("\(count)").padding().foregroundColor(Color.black).font(.system(size:80))
+                        .frame(width: 200, height: 200, alignment: .center).border(Color.white)
+                }
+                HStack{
+                    Button(action:
+                            {
+                                self.count-=1
+                            }){
+                        Text("-").padding().foregroundColor(Color.black).font(.system(size: 60)).frame(width: 200, height: 100, alignment: .center).background(Color.red)
+                    }
+                }
+            }
+        }
+        
+        
     }
 }
 
